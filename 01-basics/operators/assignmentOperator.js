d5555cfb00af9305646ac56af3817691bed4aec2
Praveena-2003 <k.praveena1803@gmail.com>
2026-01-07 20:47:28 +0530

    add assignment operator examples

---
+++ 01-basics/operators/assignmentOperator.js
@@ -0,0 +1,47 @@
+// File: 01-basics/operators/assignmentOperator.js
+// Topic: JavaScript Assignment Operators
+// Author: Praveena
+// Module: Operators
+// Purpose: Update values using assignment operators
+
+let baseValue = 10;
+let secondValue = 3;
+
+// Assignment
+baseValue = secondValue;
+console.log("Assignment (=):", baseValue);
+
+// Reset baseValue
+baseValue = 10;
+
+// Addition Assignment
+baseValue += secondValue;
+console.log("Addition Assignment (+=):", baseValue);
+
+// Reset baseValue
+baseValue = 10;
+
+// Subtraction Assignment
+baseValue -= secondValue;
+console.log("Subtraction Assignment (-=):", baseValue);
+
+// Reset baseValue
+baseValue = 10;
+
+// Multiplication Assignment
+baseValue *= secondValue;
+console.log("Multiplication Assignment (*=):", baseValue);
+
+// Reset baseValue
+baseValue = 10;
+
+// Division Assignment
+baseValue /= secondValue;
+console.log("Division Assignment (/=):", baseValue);
+
+// Reset baseValue
+baseValue = 10;
+
+// Modulus Assignment
+baseValue %= secondValue;
+console.log("Modulus Assignment (%=):", baseValue);
