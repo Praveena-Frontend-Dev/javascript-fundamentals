b9963f1515fedb2dae465ab0c31cff4f5f2978c4
Praveena-2003 <k.praveena1803@gmail.com>
2026-01-07 20:44:28 +0530

    Add arithmetic Operator

---
+++ 01-basics/operators/arithmeticOperator.js
@@ -0,0 +1,36 @@
+// File: 01-basics/operators/arithmeticOperator.js
+// Topic: JavaScript Arithmetic Operators
+// Author: Praveena
+// Module: Operators
+// Purpose: Perform basic mathematical operations
+
+let baseValue = 10;
+let secondValue = 3;
+
+// Addition
+const additionValue = baseValue + secondValue;
+console.log("Addition:", additionValue);
+
+// Subtraction
+const subtractionValue = baseValue - secondValue;
+console.log("Subtraction:", subtractionValue);
+
+// Multiplication
+const multiplicationValue = baseValue * secondValue;
+console.log("Multiplication:", multiplicationValue);
+
+// Division
+const divisionValue = baseValue / secondValue;
+console.log("Division:", divisionValue);
+
+// Modulus (Remainder)
+const remainderValue = baseValue % secondValue;
+console.log("Modulus:", remainderValue);
+
+// Increment
+baseValue++;
+console.log("Incremented baseValue:", baseValue);
+
+// Decrement
+secondValue--;
+console.log("Decremented secondValue:", secondValue);
